0e2605e62e9b4da4d1175d893d7f2c6e91a4e222
Merlin <knight_j_k@mail.ru>
2020-12-24 14:05:39 +0500

    Сделал направления константами

--- Robot.py
+++ Robot.py
@@ -1,13 +1,21 @@
-import random
-import timeit
+from enum import Enum
+from random import choice
+from timeit import default_timer
+
+
+class Direction(Enum):
+    UP = 'UP'
+    DOWN = 'DOWN'
+    RIGHT = 'RIGHT'
+    LEFT = 'LEFT'
 
 
 class Robot:
 
     def __init__(self, finish_x, finish_y):
-        self.position_x = random.choice([x for x in range(-100, 100 + 1)])
-        self.position_y = random.choice([y for y in range(-100, 100 + 1)])
-        self.sight_side = random.choice(['Up', 'Down', 'Left', 'Right'])
+        self.position_x = choice([x for x in range(-100, 100 + 1)])
+        self.position_y = choice([y for y in range(-100, 100 + 1)])
+        self.direction = choice(list(Direction))
         self.finish_x = int(finish_x)
         self.finish_y = int(finish_y)
 
@@ -17,45 +25,45 @@ class Robot:
 
     @property
     def get_direction(self):
-        return self.sight_side
+        return self.direction
 
     @property
     def get_self_coordinate(self):
         return self.position_x, self.position_y
 
     def turn_left(self):
-        if self.sight_side == 'Up':
-            self.sight_side = 'Left'
-        elif self.sight_side == 'Left':
-            self.sight_side = 'Down'
-        elif self.sight_side == 'Down':
-            self.sight_side = 'Right'
-        elif self.sight_side == 'Right':
-            self.sight_side = 'Up'
+        if self.direction == Direction.UP:
+            self.direction = Direction.LEFT
+        elif self.direction == Direction.LEFT:
+            self.direction = Direction.DOWN
+        elif self.direction == Direction.DOWN:
+            self.direction = Direction.RIGHT
+        elif self.direction == Direction.RIGHT:
+            self.direction = Direction.UP
 
     def turn_right(self):
-        if self.sight_side == 'Up':
-            self.sight_side = 'Right'
-        elif self.sight_side == 'Right':
-            self.sight_side = 'Down'
-        elif self.sight_side == 'Down':
-            self.sight_side = 'Left'
-        elif self.sight_side == 'Left':
-            self.sight_side = 'Up'
+        if self.direction == Direction.UP:
+            self.direction = Direction.RIGHT
+        elif self.direction == Direction.RIGHT:
+            self.direction = Direction.DOWN
+        elif self.direction == Direction.DOWN:
+            self.direction = Direction.LEFT
+        elif self.direction == Direction.LEFT:
+            self.direction = Direction.UP
 
     def step_forward(self):
-        if self.sight_side == 'Up':
+        if self.direction == Direction.UP:
             self.position_y += 1
-        elif self.sight_side == 'Left':
+        elif self.direction == Direction.LEFT:
             self.position_x -= 1
-        elif self.sight_side == 'Down':
+        elif self.direction == Direction.DOWN:
             self.position_y -= 1
-        elif self.sight_side == 'Right':
+        elif self.direction == Direction.RIGHT:
             self.position_x += 1
 
 
 def game():
-    robot = Robot(finish_x=16, finish_y=-78)
+    robot = Robot(finish_x=0, finish_y=0)
     print(f'My starting coordinates: {robot.get_self_coordinate}')
     print(f'I look {robot.get_direction}')
     print(f'I need to reach the coordinates: {robot.coordinate_finish}')
@@ -63,19 +71,19 @@ def game():
 
         while robot.get_self_coordinate[0] != robot.coordinate_finish[0]:
             if robot.get_self_coordinate[0] > robot.coordinate_finish[0]:
-                while robot.get_direction != 'Left':
+                while robot.get_direction != Direction.LEFT:
                     robot.turn_left()
             else:
-                while robot.get_direction != 'Right':
+                while robot.get_direction != Direction.RIGHT:
                     robot.turn_left()
             robot.step_forward()
 
         while robot.get_self_coordinate[1] != robot.coordinate_finish[1]:
             if robot.get_self_coordinate[1] > robot.coordinate_finish[1]:
-                while robot.get_direction != 'Down':
+                while robot.get_direction != Direction.DOWN:
                     robot.turn_left()
             else:
-                while robot.get_direction != 'Up':
+                while robot.get_direction != Direction.UP:
                     robot.turn_left()
             robot.step_forward()
 
@@ -84,7 +92,7 @@ def game():
     print(f'Finish coordinates: {robot.coordinate_finish}')
 
 
-start = timeit.default_timer()
+start = default_timer()
 game()
-finish = timeit.default_timer()
+finish = default_timer()
 print(f'I walked for {finish - start} seconds')

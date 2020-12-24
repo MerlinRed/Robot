d968ebc384e463503ca3ba8e3411292ab1ccf582
Merlin <knight_j_k@mail.ru>
2020-12-24 13:35:28 +0500

    Сделал рефакторинг кода

--- Robot.py
+++ Robot.py
@@ -1,12 +1,13 @@
+import random
 import timeit
 
 
 class Robot:
 
-    def __init__(self, position_x, position_y, sight_side, finish_x, finish_y):
-        self.position_x = int(position_x)
-        self.position_y = int(position_y)
-        self.sight_side = str(sight_side).title()
+    def __init__(self, finish_x, finish_y):
+        self.position_x = random.choice([x for x in range(-100, 100 + 1)])
+        self.position_y = random.choice([y for y in range(-100, 100 + 1)])
+        self.sight_side = random.choice(['Up', 'Down', 'Left', 'Right'])
         self.finish_x = int(finish_x)
         self.finish_y = int(finish_y)
 
@@ -54,90 +55,36 @@ class Robot:
 
 
 def game():
-    robot = Robot(position_x=65.45, position_y=4, sight_side='lEFT', finish_x=-8, finish_y=10/2)
-    sight_side = ['Up', 'Down', 'Left', 'Right']
+    robot = Robot(finish_x=16, finish_y=-78)
     print(f'My starting coordinates: {robot.get_self_coordinate}')
+    print(f'I look {robot.get_direction}')
     print(f'I need to reach the coordinates: {robot.coordinate_finish}')
     while robot.coordinate_finish != robot.get_self_coordinate:
 
-        if robot.coordinate_finish[0] < 0:
-            if robot.get_direction in sight_side:
-                if robot.get_self_coordinate[0] > robot.coordinate_finish[0]:
-                    while robot.get_self_coordinate[0] != robot.coordinate_finish[0]:
-                        while robot.get_direction != 'Left':
-                            robot.turn_left()
-                        robot.step_forward()
-                else:
-                    while robot.get_self_coordinate[0] != robot.coordinate_finish[0]:
-                        while robot.get_direction != 'Right':
-                            robot.turn_left()
-                        robot.step_forward()
-        elif robot.coordinate_finish[0] == 0:
-            if robot.get_direction in sight_side:
-                while robot.get_self_coordinate[0] != robot.coordinate_finish[0]:
-                    while robot.get_self_coordinate != 'Right' or 'Left':
-                        robot.turn_right()
-                    if robot.get_direction == 'Right':
-                        robot.turn_left()
-                        robot.turn_left()
-                    elif robot.get_direction == 'Left':
-                        robot.turn_right()
-                        robot.turn_right()
-                    robot.step_forward()
-        elif robot.coordinate_finish[0] > 0:
-            if robot.get_direction in sight_side:
-                if robot.get_self_coordinate[0] > robot.coordinate_finish[0]:
-                    while robot.get_self_coordinate[0] != robot.coordinate_finish[0]:
-                        while robot.get_direction != 'Left':
-                            robot.turn_right()
-                        robot.step_forward()
-                else:
-                    while robot.get_self_coordinate[0] != robot.coordinate_finish[0]:
-                        while robot.get_direction != 'Right':
-                            robot.turn_right()
-                        robot.step_forward()
+        while robot.get_self_coordinate[0] != robot.coordinate_finish[0]:
+            if robot.get_self_coordinate[0] > robot.coordinate_finish[0]:
+                while robot.get_direction != 'Left':
+                    robot.turn_left()
+            else:
+                while robot.get_direction != 'Right':
+                    robot.turn_left()
+            robot.step_forward()
 
-        if robot.coordinate_finish[1] < 0:
-            if robot.get_direction in sight_side:
-                if robot.get_self_coordinate[1] > robot.coordinate_finish[1]:
-                    while robot.get_self_coordinate[1] != robot.coordinate_finish[1]:
-                        while robot.get_direction != 'Down':
-                            robot.turn_left()
-                        robot.step_forward()
-                else:
-                    while robot.get_self_coordinate[1] != robot.coordinate_finish[1]:
-                        while robot.get_direction != 'Up':
-                            robot.turn_left()
-                        robot.step_forward()
-        elif robot.coordinate_finish[1] == 0:
-            if robot.get_direction in sight_side:
-                while robot.get_self_coordinate[1] != robot.coordinate_finish[1]:
-                    while robot.get_direction != 'Up' or 'Down':
-                        robot.turn_left()
-                    if robot.get_direction == 'Up':
-                        robot.turn_left()
-                        robot.turn_left()
-                    elif robot.get_direction == 'Down':
-                        robot.turn_right()
-                        robot.turn_right()
-                    robot.step_forward()
-        elif robot.coordinate_finish[1] > 0:
-            if robot.get_direction in sight_side:
-                if robot.get_self_coordinate[1] > robot.coordinate_finish[1]:
-                    while robot.get_self_coordinate[1] != robot.coordinate_finish[1]:
-                        while robot.get_direction != 'Down':
-                            robot.turn_right()
-                        robot.step_forward()
-                else:
-                    while robot.get_self_coordinate[1] != robot.coordinate_finish[1]:
-                        while robot.get_direction != 'Up':
-                            robot.turn_right()
-                        robot.step_forward()
+        while robot.get_self_coordinate[1] != robot.coordinate_finish[1]:
+            if robot.get_self_coordinate[1] > robot.coordinate_finish[1]:
+                while robot.get_direction != 'Down':
+                    robot.turn_left()
+            else:
+                while robot.get_direction != 'Up':
+                    robot.turn_left()
+            robot.step_forward()
+
     print(f'My coordinates: {robot.get_self_coordinate}')
+    print(f'I look {robot.get_direction}')
     print(f'Finish coordinates: {robot.coordinate_finish}')
 
 
 start = timeit.default_timer()
 game()
 finish = timeit.default_timer()
-print(f'I walked for {finish-start} seconds')
+print(f'I walked for {finish - start} seconds')
